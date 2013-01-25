a81bdbfc630255546bea93f4b59a55d3935a0844
Elvis Nunez <elvisnunez@me.com>
2013-01-25 10:19:21 -0500

    Changed folder location.

---
+++ FirstHomework/FirstHomework/Controllers/CTGEmptyViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTGEmptyViewController.h
+//  FirstHomework
+//
+//  Created by Elvis Nunez on 1/25/13.
+//  Copyright (c) 2013 CodingTogether. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTGEmptyViewController : UIViewController
+
+@end
---
+++ FirstHomework/FirstHomework/Controllers/CTGEmptyViewController.m
@@ -0,0 +1,38 @@
+//
+//  CTGEmptyViewController.m
+//  FirstHomework
+//
+//  Created by Elvis Nunez on 1/25/13.
+//  Copyright (c) 2013 CodingTogether. All rights reserved.
+//
+
+#import "CTGEmptyViewController.h"
+
+@interface CTGEmptyViewController ()
+
+@end
+
+@implementation CTGEmptyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
